5b507e57ffb31a07738a2524d87b3c2e8888929f
IgorJanicevic <milkomilkic@yahoo.com>
2024-05-11 20:59:49 +0200

    HomeView
    Navigation bar..

---
+++ NetworkService/NetworkService/NetworkService/Model/Object.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public enum Types
+    {
+        Panel,Generator
+    }
+    public class Object
+    {
+        public int Id {  get; set; }
+        public Types Type { get; set; }
+        public string Name { get; set; }
+
+        public Object()
+        {
+        }
+
+        public Object(int id,string name, Types type)
+        {
+            Id = id;
+            Type = type;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Type} {Name}";
+        }
+    }
+}
